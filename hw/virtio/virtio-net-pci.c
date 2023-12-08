7c12f395f1cf94f3957df7254925b4369309db5c
liaocj <liaocj@yusur.tech>
2023-12-08 19:50:38 +0800

    add  virtio vf

--- hw/virtio/virtio-net-pci.c
+++ hw/virtio/virtio-net-pci.c
@@ -25,7 +25,7 @@
 #include "qom/object.h"
 
 typedef struct VirtIONetPCI VirtIONetPCI;
-
+typedef struct VirtIONetVfPCI VirtIONetVfPCI;
 /*
  * virtio-net-pci: This extends VirtioPCIProxy.
  */
@@ -33,9 +33,22 @@ typedef struct VirtIONetPCI VirtIONetPCI;
 DECLARE_INSTANCE_CHECKER(VirtIONetPCI, VIRTIO_NET_PCI,
                          TYPE_VIRTIO_NET_PCI)
 
+#define TYPE_VIRTIO_NET_PCI_VF "virtio-net-pci-vf-base"
+DECLARE_INSTANCE_CHECKER(VirtIONetPCI, VIRTIO_NET_PCI,
+                         TYPE_VIRTIO_NET_PCI)
+
+OBJECT_DECLARE_SIMPLE_TYPE(VirtIONetVfPCI, VIRTIONETVF)
+
 struct VirtIONetPCI {
     VirtIOPCIProxy parent_obj;
     VirtIONet vdev;
+};
+
+struct VirtIONetVfPCI {
+    PCIDevice parent_obj;
+
+    MemoryRegion mmio;
+    MemoryRegion msix;
 };
 
 static Property virtio_net_properties[] = {
@@ -45,6 +58,14 @@ static Property virtio_net_properties[] = {
                        DEV_NVECTORS_UNSPECIFIED),
     DEFINE_PROP_END_OF_LIST(),
 };
+
+#define VIRTIO_NET_VF_MMIO_SIZE      (16 * 1024)
+#define VIRTIO_NET_VF_MSIX_SIZE      (16 * 1024)
+#define VIRTIO_NET_CAP_SRIOV_OFFSET  (0x160)
+#define VIRTIO_NET_CAP_ARI_OFFSET    (0x150)
+#define VIRTIO_NET_VF_DEV_ID         (0x1041)
+#define VIRTIO_NET_VF_OFFSET         (0x80)
+#define VIRTIO_NET_VF_STRIDE         (2)
 
 static void virtio_net_pci_realize(VirtIOPCIProxy *vpci_dev, Error **errp)
 {
@@ -61,6 +82,22 @@ static void virtio_net_pci_realize(VirtIOPCIProxy *vpci_dev, Error **errp)
 
     virtio_net_set_netclient_name(&dev->vdev, qdev->id,
                                   object_get_typename(OBJECT(qdev)));
+
+    PCIDevice *pci_dev = &vpci_dev->pci_dev;
+    pcie_ari_init(pci_dev, VIRTIO_NET_CAP_ARI_OFFSET);
+
+    pcie_sriov_pf_init(pci_dev, VIRTIO_NET_CAP_SRIOV_OFFSET, TYPE_VIRTIO_NET_PCI,
+        VIRTIO_NET_VF_DEV_ID, 8, 8,
+        VIRTIO_NET_VF_OFFSET, VIRTIO_NET_VF_STRIDE);
+
+    pcie_sriov_pf_init_vf_bar(pci_dev, vpci_dev->modern_mem_bar_idx,
+        PCI_BASE_ADDRESS_MEM_TYPE_64 | PCI_BASE_ADDRESS_MEM_PREFETCH,
+        VIRTIO_NET_VF_MMIO_SIZE);
+
+    pcie_sriov_pf_init_vf_bar(pci_dev, vpci_dev->msix_bar_idx,
+        PCI_BASE_ADDRESS_MEM_TYPE_64 | PCI_BASE_ADDRESS_MEM_PREFETCH,
+        VIRTIO_NET_VF_MSIX_SIZE);
+
     qdev_realize(vdev, BUS(&vpci_dev->bus), errp);
 }
 
@@ -90,6 +127,110 @@ static void virtio_net_pci_instance_init(Object *obj)
                               "bootindex");
 }
 
+static void virtio_net_pci_vf_qdev_reset_hold(Object *obj)
+{
+    PCIDevice *vf = PCI_DEVICE(obj);
+
+    igb_vf_reset(pcie_sriov_get_pf(vf), pcie_sriov_vf_number(vf));
+}
+
+static void virtio_net_pci_vf_pci_uninit(PCIDevice *dev)
+{
+    VirtIONetVfPCI *s = VIRTIONETVF(dev);
+
+    pcie_aer_exit(dev);
+    pcie_cap_exit(dev);
+    msix_unuse_all_vectors(dev);
+    msix_uninit(dev, &s->msix, &s->msix);
+}
+
+static void virtio_net_pci_vf_write_config(PCIDevice *dev, uint32_t addr, uint32_t val,
+    int len)
+{
+    pci_default_write_config(dev, addr, val, len);
+    if (object_property_get_bool(OBJECT(pcie_sriov_get_pf(dev)),
+                                 "x-pcie-flr-init", &error_abort)) {
+        pcie_cap_flr_write_config(dev, addr, val, len);
+    }
+}
+
+static uint64_t virtio_net_pci_vf_mmio_read(void *opaque, hwaddr addr, unsigned size)
+{
+    PCIDevice *vf = PCI_DEVICE(opaque);
+    PCIDevice *pf = pcie_sriov_get_pf(vf);
+
+    addr = vf_to_pf_addr(addr, pcie_sriov_vf_number(vf), false);
+    return addr == HWADDR_MAX ? 0 : igb_mmio_read(pf, addr, size);
+}
+
+static void virtio_net_pci_vf_mmio_write(void *opaque, hwaddr addr, uint64_t val,
+    unsigned size)
+{
+    PCIDevice *vf = PCI_DEVICE(opaque);
+    PCIDevice *pf = pcie_sriov_get_pf(vf);
+
+    addr = vf_to_pf_addr(addr, pcie_sriov_vf_number(vf), true);
+    if (addr != HWADDR_MAX) {
+        virtio_pci_device_write(pf, addr, val, size);
+    }
+}
+
+static const MemoryRegionOps mmio_ops = {
+    .read =  virtio_net_pci_vf_mmio_read,
+    .write =  virtio_net_pci_vf_mmio_write,
+    .endianness = DEVICE_LITTLE_ENDIAN,
+    .impl = {
+        .min_access_size = 4,
+        .max_access_size = 4,
+    },
+};
+
+static void virtio_net_pci_vf_pci_realize(PCIDevice *dev, Error **errp)
+{
+    VirtIONetVfPCI *s = VIRTIONETVF(dev);
+    int ret;
+    int i;
+
+    dev->config_write = virtio_net_pci_vf_write_config;
+    int mmio_bar_id = VIRTIO_PCI(dev)->modern_mem_bar_idx;
+    int msix_bar_id = VIRTIO_PCI(dev)->msix_bar_idx;
+    int nvectors = VIRTIO_PCI(dev)->nvectors;
+
+    memory_region_init_io(&s->mmio, OBJECT(dev), &mmio_ops, s, "virtio_net_pci_vf-mmio",
+        VIRTIO_NET_VF_MMIO_SIZE);
+    pcie_sriov_vf_register_bar(dev, mmio_bar_id, &s->mmio);
+    memory_region_init(&s->msix, OBJECT(dev), "virtio_net_pci_vf-msix",
+        VIRTIO_NET_VF_MSIX_SIZE);
+    pcie_sriov_vf_register_bar(dev, msix_bar_id, &s->msix);
+
+    ret = msix_init(dev, nvectors, &s->msix, msix_bar_id,
+                    0, &s->msix,
+                    mmio_bar_id, 0x2000,
+                    0x70, errp);
+    if (ret) {
+        return;
+    }
+
+    for (i = 0; i < nvectors; i++) {
+        msix_vector_use(dev, i);
+    }
+
+    if (pcie_endpoint_cap_init(dev, 0xa0) < 0) {
+        hw_error("Failed to initialize PCIe capability");
+    }
+
+    if (object_property_get_bool(OBJECT(pcie_sriov_get_pf(dev)),
+                                 "x-pcie-flr-init", &error_abort)) {
+        pcie_cap_flr_init(dev);
+    }
+
+    if (pcie_aer_init(dev, 1, 0x100, 0x40, errp) < 0) {
+        hw_error("Failed to initialize AER capability");
+    }
+
+    pcie_ari_init(dev, 0x150);
+}
+
 static const VirtioPCIDeviceTypeInfo virtio_net_pci_info = {
     .base_name             = TYPE_VIRTIO_NET_PCI,
     .generic_name          = "virtio-net-pci",
@@ -100,9 +241,42 @@ static const VirtioPCIDeviceTypeInfo virtio_net_pci_info = {
     .class_init    = virtio_net_pci_class_init,
 };
 
+static void virtio_net_pci_vf_class_init(ObjectClass *class, void *data)
+{
+    DeviceClass *dc = DEVICE_CLASS(class);
+    PCIDeviceClass *c = PCI_DEVICE_CLASS(class);
+    ResettableClass *rc = RESETTABLE_CLASS(class);
+
+    c->realize = virtio_net_pci_vf_pci_realize;
+    c->exit = virtio_net_pci_vf_pci_uninit;
+    c->vendor_id = PCI_VENDOR_ID_REDHAT_QUMRANET;
+    c->device_id = PCI_DEVICE_ID_VIRTIO_NET;
+    c->revision = VIRTIO_PCI_ABI_VERSION;
+    c->class_id = PCI_CLASS_NETWORK_ETHERNET;
+
+    rc->phases.hold = virtio_net_pci_vf_qdev_reset_hold;
+
+    dc->desc = "virtio net pci virtual function";
+    dc->user_creatable = false;
+
+    set_bit(DEVICE_CATEGORY_NETWORK, dc->categories);
+}
+
+static const TypeInfo virtio_net_pci_vf_info = {
+    .name = TYPE_VIRTIO_NET_PCI_VF,
+    .parent = TYPE_PCI_DEVICE,
+    .instance_size = sizeof(VirtIONetVfPCI),
+    .class_init = virtio_net_pci_vf_class_init,
+    .interfaces = (InterfaceInfo[]) {
+        { INTERFACE_PCIE_DEVICE },
+        { }
+    },
+};
+
 static void virtio_net_pci_register(void)
 {
     virtio_pci_types_register(&virtio_net_pci_info);
+    type_register_static(&virtio_net_pci_vf_info);
 }
 
 type_init(virtio_net_pci_register)
