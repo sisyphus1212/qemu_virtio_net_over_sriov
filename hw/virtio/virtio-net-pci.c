58dec79771977ef60633434041427d2d0df1e211
liaocj <liaocj@yusur.tech>
2023-12-13 15:17:43 +0800

    fix err

--- hw/virtio/virtio-net-pci.c
+++ hw/virtio/virtio-net-pci.c
@@ -307,39 +307,6 @@ static void virtio_net_pci_vf_pci_realize(PCIDevice *dev, Error **errp)
     memcpy(dev->config + offset + PCI_CAP_FLAGS, &cap.cap_len,
         cap.cap_len - PCI_CAP_FLAGS);
 
-    cap.cfg_type = 0;
-    cap.bar = 4;
-    cap.offset = 0x0;
-    cap.length = 0x1000;
-    cap.cap_len = sizeof cap;
-    offset = pci_add_capability(dev, PCI_CAP_ID_VNDR, 0,
-                                cap.cap_len, &error_abort);
-
-    memcpy(dev->config + offset + PCI_CAP_FLAGS, &cap.cap_len,
-        cap.cap_len - PCI_CAP_FLAGS);
-
-    cap.cfg_type = 3;
-    cap.bar = 4;
-    cap.offset = 0x0;
-    cap.length = 0x1000;
-    cap.cap_len = sizeof cap;
-    offset = pci_add_capability(dev, PCI_CAP_ID_VNDR, 0,
-                                cap.cap_len, &error_abort);
-    memcpy(dev->config + offset + PCI_CAP_FLAGS, &cap.cap_len,
-        cap.cap_len - PCI_CAP_FLAGS);
-
-    cap.cfg_type = 4;
-    cap.bar = 4;
-    cap.offset = 0x0;
-    cap.length = 0x1000;
-    cap.cap_len = sizeof cap;
-    //offset = pci_add_capability(dev, PCI_CAP_ID_VNDR, 0,
-    //                            cap.cap_len, &error_abort);
-//
-    //memcpy(dev->config + offset + PCI_CAP_FLAGS, &cap.cap_len,
-    //    cap.cap_len - PCI_CAP_FLAGS);
-
-    //virtio_net_pci_vf_pci_cap_init(&(s->vf), dev);
     ret = msix_init(dev, nvectors, &s->msix, msix_bar_id,
                     0, &s->msix,
                     mmio_bar_id, 0x2000,
